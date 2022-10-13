02cf32d84f045fbfadca7ff55ec2523eeee7ef4f
Edward Iga Kigongo <igakigongo@gmail.com>
2022-10-13 17:22:53 +0200

    update event cost

--- Norriq.DataVerse.EventsManager.Plugins/CalculateEventCostPlugin.cs
+++ Norriq.DataVerse.EventsManager.Plugins/CalculateEventCostPlugin.cs
@@ -9,39 +9,26 @@ namespace Norriq.DataVerse.EventsManager.Plugins
 {
     public class CalculateEventCostPlugin : BasePlugin
     {
-        private readonly Money CongressCost = new Money(2000);
-        private readonly Money MarketingCost = new Money(8000);
-
         protected override void Execute(IPluginExecutionContext context, IOrganizationService service, AppInsightsTracingService tracingService)
-        {
-            switch(context.MessageName)
-            {
-                case MsgCreate:
-                    SetPriceOnCreate(context, service, tracingService);
-                    break;
-
-                case MsgUpdate:
-                    SetPriceOnUpdate(context, service, tracingService);
-                    break;
-            }
-        }
-
-        private void SetPriceOnCreate(IPluginExecutionContext context, IOrganizationService service, AppInsightsTracingService tracingService)
         {
             if (!string.Equals(_target.LogicalName, "nrq_event", StringComparison.OrdinalIgnoreCase))
                 return;
 
-            tracingService.LogTrace($"{nameof(CalculateEventCostPlugin)}: Primary Entity ID = {1}, Primary Entity Name: {0}",
-                context.PrimaryEntityId, context.PrimaryEntityName);
-
-            var nrqEvent = _target.ToEntity<nrq_Event>();
-
-            var evtType = nrq_EventType.Retrieve(service, nrqEvent.nrq_EventTypeId.Id, x => x.nrq_Cost);
-            nrqEvent.nrq_TotalCost = evtType.nrq_Cost;
+            tracingService.LogTrace($"{nameof(CalculateEventCostPlugin)}: Action = {context.MessageName}");
+            switch (context.MessageName)
+            {
+                case MsgCreate:
+                case MsgUpdate:
+                    OnCreateOrUpdateSetEventCost(service);
+                    break;
+            }
         }
 
-        private void SetPriceOnUpdate(IPluginExecutionContext context, IOrganizationService service, AppInsightsTracingService tracingService)
+        private void OnCreateOrUpdateSetEventCost(IOrganizationService service)
         {
+            var nrqEvent = _target.ToEntity<nrq_Event>();
+            var evtType = nrq_EventType.Retrieve(service, nrqEvent.nrq_EventTypeId.Id, x => x.nrq_Cost);
+            nrqEvent.nrq_TotalCost = evtType.nrq_Cost;
         }
     }
 }
--- Norriq.DataVerse.EventsManager.XrmContext/Models/EventType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xrm.Sdk;
-
-namespace Norriq.DataVerse.EventsManager.XrmContext.Models
-{
-    public partial class nrq_EventType
-    {
-        public string Name => nrq_EventType_1;
-    }
-}
